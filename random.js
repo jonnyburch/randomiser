a9c62d089844d81fee28684d77eb7dff59c4462b
Jonny Burch <jonny@osper.com>
2014-10-18 22:09:23 +0100

    Got it working, need to work out how to get a string in another file into the var

--- random.js
+++ random.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
-	
-	var array1 = ['Moths', 'Skulls', 'Winter'];
+
+	var mediums = "students artists photographers dancers actors bankers";
+	var verbs = "looking dancing blinking crowing"
+	var subjects = "ponies skulls moths fruit bananas"
+
+	var array1 = mediums.split(" ");
+	var array2 = verbs.split(" ");
+	var array3 = subjects.split(" ");
 
 	var $rand1 = array1[Math.floor(Math.random() * array1.length)];
+	var $rand2 = array2[Math.floor(Math.random() * array2.length)];
+	var $rand3 = array3[Math.floor(Math.random() * array3.length)];
 
-	$('#randomise').onClick(function(){
-		$('#array1').append($rand1);
-	});
+	document.querySelector('.array1').innerHTML = $rand1;
+	document.querySelector('.array2').innerHTML = $rand2;
+	document.querySelector('.array3').innerHTML = $rand3;
 
 });
 
